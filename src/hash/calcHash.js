6adb68418c67569aeb7f2ad776f3349a53fc8c46
Dmitry <papkovdmitry@gmail.com>
2024-10-02 10:51:03 +0500

    subtask: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(import.meta.dirname,'files', 'fileToCalculateHashFor.txt')
+    const content = await readFile(filePath)
+
+    const hash = createHash('sha256').update(content).digest('hex')
+
+    console.log(hash)
 };
 
 await calculateHash();
